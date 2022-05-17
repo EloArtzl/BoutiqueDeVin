d3ffe85a1a7ce49d58222e77649083980c2d7e69
EloArtzl <ac.eloise@gmail.com>
2022-05-17 14:16:43 +0200

    before merge

---
+++ src/Admin.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Admin extends Personne {
+	public Admin(String nom, String prenom, String email) {
+		super(nom, prenom, email);
+	}
+	public void addVin() {
+		//code
+	}
+	public void deleteVin() {
+		//code
+	}
+	public void searchVin() {
+		//code
+	}
+	public void modifyVin() {
+		//code
+	}
+}
